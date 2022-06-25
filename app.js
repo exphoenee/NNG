362478251302107808303c1dd599f2150be5f01e
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 16:07:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-//let balconyLeft = new Auditorium([5, 5, 5, 5]);
-//console.log(balconyLeft);
-//let row = new Row();
-let seat = new Seat({
-  number: 0,
-  ticketPrice: 2000,
-  occupied: true,
-  guestName: "Jani",
-});
-console.log(seat);
+let row = new Row([0, 0, 1, 1, 2, 2]);
+console.log(row);
