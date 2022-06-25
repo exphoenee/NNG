7917a89cc9d574fc18f5251506277e8af309b31d
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 15:23:00 +0200

    Update auditorium.js

--- auditorium.js
+++ auditorium.js
@@ -1,5 +1,11 @@
 class Auditorium {
   constructor(rows) {
+    this.seatRow = [];
+    rows.forEach((rowLength, rowNr) => {
+      for (let seatNr = 0; seatNr < rowLength; seatNr++) {
+        this.seatRow[rowNr][seatNr] = new Seat({ number: seatNr });
+      }
+    });
     return this;
   }
 }
