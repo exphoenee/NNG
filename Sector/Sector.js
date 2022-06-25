6d9cf8849f326327a943892b4d22f1f7f51a8cc1
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 17:49:22 +0200

    Update Sector.js

--- Sector/Sector.js
+++ Sector/Sector.js
@@ -1,6 +1,7 @@
 class Sector {
-  constructor(rows) {
+  constructor({ rows, name }) {
     this.rows = [];
+    this.name = name;
     rows.forEach((rowConf) => {
       this.rows.push(new Row(rowConf));
       return this;
@@ -8,21 +9,40 @@ class Sector {
   }
 
   render() {
+    const sectorContainer = createDOMElem({
+      tag: div,
+      attrs: {
+        class: `sector-container ${this.name}`,
+      },
+      children: [
+        {
+          tag: div,
+          attrs: {
+            class: `title`,
+          },
+          children: [{ tag: p, content: this.name }],
+        },
+      ],
+    });
     this.rows.forEach((row, rowNr) => {
-      let rowContainer = createDOMElem({
+      const rowContainer = createDOMElem({
         tag: div,
         attrs: {
           class: `row rowNr-${rowNr} row-container`,
         },
+        content: `row: ${rowNr + 1}`,
+        parent: sectorContainer[0],
       });
       row.seat.forEach((seat) => {
-        console.log();
         createDOMElem({
           tag: div,
           attrs: {
-            class: `seat seatNr-${seat.number} category-${seat.seatCategory.seatCategory}`,
+            class: `seat seatNr-${seat.number} category-${
+              seat.seatCategory.seatCategory
+            } ${seat.occupied ? "occupied" : "free"}`,
           },
           parent: rowContainer[0],
+          content: seat.number + 1,
         });
       });
     });
