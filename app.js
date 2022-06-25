ca36b2a2a8b02d14a7ff69b576dbc4c376892601
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 15:39:49 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+//let balconyLeft = new Auditorium([5, 5, 5, 5]);
+//console.log(balconyLeft);
+//let row = new Row();
+let seat = new Seat({
+  number: 0,
+  ticketPrice: 2000,
+  occupied: true,
+  guestName: "Jani",
+});
+console.log(seat);
