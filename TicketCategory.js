5bbcff68080e2ddec1b6a6071f0689c02eb9dc30
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 16:08:34 +0200

    Update TicketCategory.js

--- TicketCategory.js
+++ TicketCategory.js
@@ -2,9 +2,17 @@ class TicketCategory {
   constructor(seatCategory) {
     this.seatCategory = seatCategory;
     this.priceList = { 0: 2000, 1: 3000, 2: 4000, 3: 5000 };
+    return this;
   }
 
   getPrice() {
     return this.priceList[this.seatCategory];
+  }
+  getCategory() {
+    return this.seatCategory;
+  }
+  setCategory() {
+    this.seatCategory = seatCategory;
+    return this;
   }
 }
