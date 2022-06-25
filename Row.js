f9062e1813150bbb3ffb0aea1ab4cb71710c52fe
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 16:08:16 +0200

    Update Row.js

--- Row.js
+++ Row.js
@@ -1,1 +1,9 @@
-class Row {}
+class Row {
+  constructor(rowConf) {
+    this.seat = [];
+    rowConf.forEach((category, seatNr) => {
+      this.seat.push(new Seat({ number: seatNr, seatCategory: category }));
+    });
+    return this;
+  }
+}
