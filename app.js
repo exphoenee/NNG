d126dbafc51c037b853837eea324fdfd011d14c7
exphoenee <bozzay.viktor@gmail.com>
2022-06-25 17:59:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+/*
 let balconyLeft = new Sector({
   rows: centralAuditorium,
   name: "Central Auditory",
+  mirrored: true,
 });
 balconyLeft.render();
+*/
+const auditorium = new Auditorium([
+  { rows: boxMap, name: "Box left 1.", mirrored: false },
+  { rows: boxMap, name: "Box right 1.", mirrored: true },
+]);
+auditorium.render();
