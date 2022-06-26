cc1c822d3bba2e0074f1fd8863adb623aaacb160
exphoenee <bozzay.viktor@gmail.com>
2022-06-26 14:18:25 +0200

    Update Sector.js

--- Sector/Sector.js
+++ Sector/Sector.js
@@ -13,7 +13,7 @@ class Sector {
     });
   }
 
-  render() {
+  render(parent) {
     const sectorContainer = createDOMElem({
       tag: div,
       attrs: {
@@ -22,6 +22,7 @@ class Sector {
           .replaceAll(".", "")}`,
         id: `sectorId-${this.sectorId}`,
       },
+      parent: parent,
       children: [
         {
           tag: div,
@@ -32,45 +33,9 @@ class Sector {
         },
       ],
     });
+
     this.rows.forEach((row, rowNr) => {
-      const rowContainer = createDOMElem({
-        tag: div,
-        attrs: {
-          class: `row rowNr-${rowNr} row-container`,
-        },
-        content: `row: ${rowNr + 1}`,
-        parent: sectorContainer[0],
-      });
-      const toRender = this.mirrored ? row.seat.reverse() : row.seat;
-      toRender.forEach((seat) => {
-        createDOMElem({
-          tag: div,
-          attrs: {
-            class: `seat seatNr-${seat.number} category-${
-              seat.seatCategory.seatCategory
-            } ${seat.occupied ? "occupied" : "free"}`,
-          },
-          parent: rowContainer[0],
-          content: seat.number + 1,
-          handleEvent: {
-            event: "click",
-            cb: (e) => {
-              e.preventDefault();
-              console.log("clicked", e.target);
-
-              seat.occupied ? seat.setFree() : seat.setOccupied("Lali");
-
-              if (seat.occupied) {
-                e.target.classList.add("occupied");
-                e.target.classList.remove("free");
-              } else {
-                e.target.classList.add("free");
-                e.target.classList.remove("occupied");
-              }
-            },
-          },
-        });
-      });
+      row.render(sectorContainer, rowNr, this.mirrored);
     });
   }
 }
