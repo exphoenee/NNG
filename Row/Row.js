452152b02b302c1039c9e65231dee157cc0eb7ac
exphoenee <bozzay.viktor@gmail.com>
2022-06-26 14:18:19 +0200

    Update Row.js

--- Row/Row.js
+++ Row/Row.js
@@ -8,4 +8,46 @@ class Row {
     });
     return this;
   }
+
+  render(parent, rowNr, mirrored = false) {
+    const rowContainer = createDOMElem({
+      tag: div,
+      attrs: {
+        class: `row rowNr-${rowNr} row-container`,
+      },
+      content: `row: ${rowNr + 1}`,
+      parent: parent,
+    });
+
+    const toRender = mirrored ? this.seat.reverse() : this.seat;
+    toRender.forEach((seat) => {
+      createDOMElem({
+        tag: div,
+        attrs: {
+          class: `seat seatNr-${seat.number} category-${
+            seat.seatCategory.seatCategory
+          } ${seat.occupied ? "occupied" : "free"}`,
+        },
+        parent: rowContainer,
+        content: seat.number + 1,
+        handleEvent: {
+          event: "click",
+          cb: (e) => {
+            e.preventDefault();
+            console.log("clicked", e.target);
+
+            seat.occupied ? seat.setFree() : seat.setOccupied("Lali");
+
+            if (seat.occupied) {
+              e.target.classList.add("occupied");
+              e.target.classList.remove("free");
+            } else {
+              e.target.classList.add("free");
+              e.target.classList.remove("occupied");
+            }
+          },
+        },
+      });
+    });
+  }
 }
