33c449c7038533d2e02815f13f954dd5f9d0d46b
exphoenee <bozzay.viktor@gmail.com>
2022-06-27 22:24:17 +0200

    Create Row.js

---
+++ components/Row/Row.js
@@ -0,0 +1,50 @@
+class Row {
+  constructor({ rowConf, rowNr, sectorId, sectorName }) {
+    this.seats = [];
+    this.rowNr = rowNr;
+    this.sectorId = sectorId;
+    this.seatsNumber = rowConf.length;
+    this.sectorName = sectorName;
+    rowConf.forEach((category, seatNr) => {
+      const thisSeat = new Seat({
+        seatNr: seatNr,
+        sectorId: sectorId,
+        rowNr: rowNr,
+        seatCategory: category,
+        sectorName: sectorName,
+      });
+      this.seats.push(thisSeat);
+    });
+    return this;
+  }
+
+  getOccupied() {
+    return this.seats.filter((seat) => seat.occupied);
+  }
+
+  render(parent, mirrored, offset) {
+    const rowContainer = createDOMElem({
+      tag: div,
+      attrs: {
+        class: `row rowNr-${this.rowNr} row-container`,
+      },
+      content: `${this.rowNr + 1}${trt[this.rowNr] || trt.default} row`,
+      parent: parent,
+    });
+
+    const toRender = mirrored ? this.seats.reverse() : this.seats;
+
+    toRender.forEach((seat, index) => {
+      seat.render(rowContainer);
+      if (offset > 0 && index == toRender.length - 1) {
+        for (let i = 0; i < this.rowNr - 1; i++) {
+          createDOMElem({
+            tag: div,
+            attrs: { class: "offset" },
+            parent: rowContainer,
+          });
+        }
+      }
+    });
+  }
+}
