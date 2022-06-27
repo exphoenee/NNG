bbe3be855a23bdf67add4f75224c594dcfe4c775
exphoenee <bozzay.viktor@gmail.com>
2022-06-27 21:56:24 +0200

    Update Sector.js

--- Sector/Sector.js
+++ Sector/Sector.js
@@ -10,7 +10,7 @@ class Sector {
     this.seatNumber = 0;
     this.offset = offset;
     rows.forEach((rowConf, rowNr) => {
-      const row = new Row({ rowConf, rowNr, sectorId });
+      const row = new Row({ rowConf, rowNr, sectorId, sectorName: name });
       this.seatNumber += row.seatNumber;
       this.rows.push(row);
       return this;
