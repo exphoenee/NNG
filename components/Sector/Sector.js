1b9f64e1042e3e7cbc7957a2ce763ad50c3921c6
exphoenee <bozzay.viktor@gmail.com>
2022-06-27 22:24:38 +0200

    Create Sector.js

---
+++ components/Sector/Sector.js
@@ -0,0 +1,54 @@
+class Sector {
+  constructor({ rows, name, mirrored, sectorId, vert, hor, angle, offset }) {
+    this.rows = [];
+    this.name = name;
+    this.vert = vert;
+    this.hor = hor;
+    this.angle = angle;
+    this.sectorId = sectorId;
+    this.mirrored = mirrored;
+    this.seatNumber = 0;
+    this.offset = offset;
+    rows.forEach((rowConf, rowNr) => {
+      const row = new Row({ rowConf, rowNr, sectorId, sectorName: name });
+      this.seatNumber += row.seatNumber;
+      this.rows.push(row);
+      return this;
+    });
+  }
+
+  getOccupied() {
+    return this.rows.map((row) => row.getOccupied()).flat(1);
+  }
+
+  render(parent) {
+    const sectorContainer = createDOMElem({
+      tag: div,
+      attrs: {
+        class: `sector-container ${this.name
+          .replaceAll(" ", "-")
+          .replaceAll(".", "")}`,
+        id: `sectorId-${this.sectorId}`,
+      },
+      style: {
+        top: `${this.vert}%`,
+        left: `${this.hor}%`,
+        transform: `translate(-50%,-50%) rotate(${this.angle || 0}deg)`,
+      },
+      parent: parent,
+      children: [
+        {
+          tag: div,
+          attrs: {
+            class: `title`,
+          },
+          children: [{ tag: p, content: this.name }],
+        },
+      ],
+    });
+
+    this.rows.forEach((row) => {
+      row.render(sectorContainer, this.mirrored, this.offset);
+    });
+  }
+}
