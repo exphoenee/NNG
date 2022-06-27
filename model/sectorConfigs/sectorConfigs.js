6f7439280f17757539f17fcdac884cdd237daf2d
exphoenee <bozzay.viktor@gmail.com>
2022-06-27 22:25:02 +0200

    Create sectorConfigs.js

---
+++ model/sectorConfigs/sectorConfigs.js
@@ -0,0 +1,80 @@
+boxLeft1 = {
+  rows: boxMap,
+  name: "Box left 1.",
+  mirrored: true,
+  vert: 25,
+  hor: 15,
+  angle: -90,
+};
+boxLeft2 = {
+  rows: boxMap,
+  name: "Box left 2.",
+  mirrored: true,
+  vert: 40,
+  hor: 15,
+  angle: -90,
+};
+boxLeft3 = {
+  rows: boxMap,
+  name: "Box left 3.",
+  mirrored: true,
+  vert: 55,
+  hor: 15,
+  angle: -90,
+};
+sideBalconyLeft = {
+  rows: sideBalconyMap,
+  name: "Balcony left",
+  mirrored: true,
+  vert: 75,
+  hor: 20,
+  angle: 45,
+  offset: 1,
+};
+boxRight1 = {
+  rows: boxMap,
+  name: "Box right 1.",
+  mirrored: false,
+  vert: 25,
+  hor: 85,
+  angle: 90,
+};
+boxRight2 = {
+  rows: boxMap,
+  name: "Box right 2.",
+  mirrored: false,
+  vert: 40,
+  hor: 85,
+  angle: 90,
+};
+boxRight3 = {
+  rows: boxMap,
+  name: "Box right 3.",
+  mirrored: false,
+  vert: 55,
+  hor: 85,
+  angle: 90,
+};
+sideBalconyRight = {
+  rows: sideBalconyMap,
+  name: "Balcony right",
+  mirrored: false,
+  vert: 75,
+  hor: 80,
+  angle: -45,
+  offset: 1,
+};
+midBalcony = {
+  rows: midBalconyMap,
+  name: "Balcony mid.",
+  mirrored: false,
+  vert: 80,
+  hor: 50,
+};
+centralAuditorium = {
+  rows: centralAuditoriumMap,
+  name: "Auditorium",
+  mirrored: false,
+  vert: 45,
+  hor: 50,
+};
