4bc86d392a320bc6d44b0ec6d070d441aab3f8b2
exphoenee <bozzay.viktor@gmail.com>
2022-06-28 20:25:59 +0200

    Update Row.js

--- components/Row/Row.js
+++ components/Row/Row.js
@@ -1,3 +1,7 @@
+/*
+this is the object of the rows, the object gets, the properies of the Auditorium, and the Sector as well, and pass down to the seat component also for later useage
+*/
+
 class Row {
   constructor({ rowConf, rowNr, sectorId, sectorName, sectorPreference }) {
     this.seats = [];
@@ -7,6 +11,8 @@ class Row {
     this.sectorPreference = rowConf.length - rowNr;
     this.sectorName = sectorName;
     this.sectorPreference = sectorPreference;
+
+    //here is the seats created in the rows
     rowConf.forEach((category, seatNr) => {
       const thisSeat = new Seat({
         seatNr: seatNr,
@@ -22,18 +28,24 @@ class Row {
     return this;
   }
 
+  //this method gives back all the seats in a flat array of an entire row
   getAllSeats() {
     return this.seats;
   }
+
+  //this method gives back all the occupied seats in a flat array of an entire row
   getOccupiedSeats() {
     return this.seats.filter((seat) => seat.occupied);
   }
 
+  //this method gives back all the free seats in a flat array of an entire row
   getFreeSeats() {
     return this.seats.filter((seat) => !seat.occupied);
   }
 
+  //this method rendering the row
   render(parent, mirrored, offset) {
+    //creating the row container
     const rowContainer = createDOMElem({
       tag: div,
       attrs: {
@@ -42,6 +54,7 @@ class Row {
       parent: parent,
     });
 
+    //adding the text of row numbering
     const rowNumbering = createDOMElem({
       tag: p,
       attrs: {
@@ -51,8 +64,10 @@ class Row {
       parent: rowContainer,
     });
 
+    //if the rows are mirrored in the sectror the revers the array
     const toRender = mirrored ? this.seats.reverse() : this.seats;
 
+    //give some offset to the side balcony to get similar results than on the picture shown
     toRender.forEach((seat, index) => {
       seat.render(rowContainer);
       if (offset > 0 && index == toRender.length - 1) {
