bb67e70e0f15b1b6e00006a8975c428d1308e6cb
exphoenee <bozzay.viktor@gmail.com>
2022-06-28 17:46:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,10 +12,5 @@ const auditorium = new Auditorium([
 ]);
 auditorium.render("app");
 
-const allSeats = auditorium.getAllSeats();
-const seatNr = allSeats.length;
-const reserved = Math.ceil(seatNr * 0.2);
-
-do {
-  allSeats[Math.round(Math.random() * (seatNr - 1))].setOccupied("Random");
-} while (auditorium.getOccupiedSeats().length <= reserved);
+auditorium.randomReservation(0.2);
+auditorium.optimize({ min: 2, max: 4 });
