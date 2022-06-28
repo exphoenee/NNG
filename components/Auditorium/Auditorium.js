8b313bc15c6c465d42c5b0c65de1a46d42ab58ba
exphoenee <bozzay.viktor@gmail.com>
2022-06-28 17:46:52 +0200

    Update Auditorium.js

--- components/Auditorium/Auditorium.js
+++ components/Auditorium/Auditorium.js
@@ -11,6 +11,28 @@ class Auditorium {
     return this;
   }
 
+  randomReservation(ammount = 0) {
+    if (ammount < 0.2) {
+      console.error(
+        "The amount of reserved seat must be over 20% of the entire seats!"
+      );
+    } else {
+      const allSeats = this.getAllSeats();
+      const seatNr = allSeats.length;
+      do {
+        allSeats[Math.round(Math.random() * (seatNr - 1))].setOccupied(
+          "Random"
+        );
+      } while (
+        auditorium.getOccupiedSeats().length <= Math.ceil(seatNr * ammount)
+      );
+    }
+  }
+
+  optimize({ min, max }) {
+    const occupied = this.getOccupiedSeats;
+  }
+
   getSeatNumber() {
     return this.seatNumber;
   }
