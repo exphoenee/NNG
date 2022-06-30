bcabdb8c4d94609c4cba5128f7eab8f1009a4e21
exphoenee <bozzay.viktor@gmail.com>
2022-06-30 19:20:37 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,7 @@ module.exports = {
     filename: "[name][contenthash].js",
     clean: true,
   },
-  module: {
-    rules: [
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
-    ],
-  },
+
   devtool: "source-map",
   devServer: {
     static: { directory: path.resolve(__dirname, "dist") },
