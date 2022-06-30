3e91565d27239c2f55896943f679ca5faa3b8004
exphoenee <bozzay.viktor@gmail.com>
2022-06-30 19:05:46 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: { main: path.resolve(__dirname, "src/index.js") },
-  output: { path: path.resolve(__dirname, "dist"), filename: "[name].js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+  },
   module: {
     rules: [
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
