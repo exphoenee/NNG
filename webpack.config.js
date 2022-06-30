1ec4a15764f3fdbd9f8db8b2f06128dc699ff1fa
exphoenee <bozzay.viktor@gmail.com>
2022-06-30 18:13:21 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: { main: path.resolve(__dirname, "src/index.js") },
   output: { path: path.resolve(__dirname, "dist"), filename: "[name].js" },
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Auditorium App",
+      filename: "index.html",
+    }),
+  ],
 };
